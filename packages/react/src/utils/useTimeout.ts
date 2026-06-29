f1abb601e1d375efd3ba9933b5f4aefa5fbe9168
x1ngYu <handsome.rxy@email.com>
2026-06-29 14:51:18 +0800

    feat: add Avatar component

---
+++ packages/react/src/utils/useTimeout.ts
@@ -0,0 +1,25 @@
+import { useEvent } from '@primitives-ui/utils'
+import { useEffect, useMemo, useRef } from 'react'
+
+export function useTimeout() {
+  const timerRef = useRef<NodeJS.Timeout>(null)
+
+  const clear = useEvent(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current)
+      timerRef.current = null
+    }
+  })
+
+  const start = useEvent((fn: (...args: any[]) => void, delay: number) => {
+    clear()
+    timerRef.current = setTimeout(() => {
+      timerRef.current = null
+      fn()
+    }, delay)
+  })
+
+  useEffect(() => clear, [clear])
+
+  return useMemo(() => ({ start, clear }), [start, clear])
+}
